51f3fcb847318acddfe1189c76056b7b0c0bfb4e
anzwkk <anam89737@gmail.com>
2025-01-31 00:29:06 +0300

    Added a method for adding money to the account and withdrawing money

---
+++ 14.1HW/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Account
+{
+    public string AccountName { get; set; }
+    public double Balance { get; set; }
+
+    public Account(string accountName, double initialBalance)
+    {
+        AccountName = accountName;
+        Balance = initialBalance < 0 ? 0 : initialBalance;
+    }
+
+    public void Deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+        }
+        else
+        {
+            Console.WriteLine("Сума депозиту має бути більше нуля");
+        }
+    }
+    public void TakeMoney(double amount)
+    {
+        if (amount > 0 && Balance - amount >= 0)
+        {
+            Balance -= amount;
+        }
+        else
+        {
+            Console.WriteLine("Недостатньо коштів або некоректна сума зняття");
+        }
+    }
+}
+
